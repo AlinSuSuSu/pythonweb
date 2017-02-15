cf6f528dc2daa30420333f7212f65dd4603dbe1d
qun.su <alinsususu@163.com>
2017-02-15 18:42:33 +0800

    before copy all

--- flasky_exercise1.py
+++ flasky_exercise1.py
@@ -1,57 +1,36 @@
-
-from flask_bootstrap import Bootstrap #引用bootstrap扩展
-from flask import Flask,render_template#引用模板
+from flask import Flask
+from flask import request#请求
+from flask import make_response#响应
+from flask import render_template#模板
+from flask_bootstrap import Bootstrap
 from flask_moment import Moment#本地化日期和时间
-from datetime import datetime
-#表单类
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from flask_wtf import Form
+from wtforms import StringField,SubmitField
 from wtforms.validators import Required
-#重定向和用户会话
-from flask import session, redirect, url_for
-#flash消息
-from flask import flash
-
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-#表单
-class NameForm(FlaskForm):
-    name = StringField("What's your name?",validators=[Required()])
-    submit = SubmitField('Submit')
-
-app = Flask(__name__)
-
-#配置通用密钥
-app.config['SECRET_KEY'] = 'hard to guess string'
-
-bootstrap = Bootstrap(app)
-moment = Moment(app)
 
 
-@app.route('/',methods=['GET','POST'])
+app = Flask(__name__)#Flask类的构造函数只有一个必须制定的参数，即程序主模块或包的名字
+app.config['SECRET_KEY'] = 'HARD TO GUESS STRING'#设置Flask-WTF
+bootstrap = Bootstrap(app) #初始化Flask-Bootstrap
+moment = Moment(app)#初始化Flask-moment
+#设置路由：处理URL和函数之间关系的函数成为路由
+@app.route('/')
 def index():
-
-    form = NameForm()
-    if form.validate_on_submit():
-         old_name = session.get('name')
-         if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
-         session['name'] = form.name.data
-         return redirect(url_for('index'))
-    return render_template('index.html',form=form,name=session.get('name'), known = session.get['known',False],current_time=datetime.utcnow())
+    '''user_agent= request.headers.get('User-Agent')
+    response = make_response('this is a cookie')
+    response.set_cookie('answer','42')
+    #return response'''
+    return render_template('index.html')
 
 
+@app.route('/user/<name1>')
+def user(name1):
+    return render_template('user.html',name = name1)
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name1=name)
-
-#自定义错误页面
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'),404
 
-
-
+#启动服务器
 if __name__ == '__main__':
-    app.run(debug=True,port=7775)
+    app.run(debug=True,port = 7775)#设置端口号为7775
