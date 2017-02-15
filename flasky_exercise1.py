9cdfd78895077e7962c17a269cd016cb736e5f7e
qun.su <alinsususu@163.com>
2017-02-15 18:54:33 +0800

    index ok

--- flasky_exercise1.py
+++ flasky_exercise1.py
@@ -4,23 +4,38 @@ from flask import make_response#响应
 from flask import render_template#模板
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment#本地化日期和时间
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField
 from wtforms.validators import Required
+#重定向和用户会话
+from flask import session, redirect, url_for
+#flash消息
+from flask import flash
+from datetime import datetime
 
+
+
+#表单
+class NameForm(FlaskForm):
+    name = StringField("What's your name?",validators=[Required()])
+    submit = SubmitField('Submit')
 
 app = Flask(__name__)#Flask类的构造函数只有一个必须制定的参数，即程序主模块或包的名字
 app.config['SECRET_KEY'] = 'HARD TO GUESS STRING'#设置Flask-WTF
 bootstrap = Bootstrap(app) #初始化Flask-Bootstrap
 moment = Moment(app)#初始化Flask-moment
+
+
+
 #设置路由：处理URL和函数之间关系的函数成为路由
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
-    '''user_agent= request.headers.get('User-Agent')
-    response = make_response('this is a cookie')
-    response.set_cookie('answer','42')
-    #return response'''
-    return render_template('index.html')
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data=''
+    return render_template('index.html',form=form,current_time=datetime.utcnow())
 
 
 @app.route('/user/<name1>')
